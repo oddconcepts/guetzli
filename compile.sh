c2664e04c2d1d9353cd28f447b70d0e45cc1e70e
Tee Jung <taeyoung@oddconcepts.kr>
2018-08-10 17:11:25 +0900

    create destination directory if not exists

--- compile.sh
+++ compile.sh
@@ -6,6 +6,8 @@ echo $2 -G
 
 nvcc -D__USE_OPENCL__ -I"./" -I"/usr/local/cuda/include" -ftz=true -prec-div=false -prec-sqrt=false -arch=compute_30 -O3 --machine $1 $2 -ptx -o clguetzli/clguetzli.cu.ptx$1  clguetzli/clguetzli.cu
 
+mkdir -p bin/Release/clguetzli
+
 #copy to ./bin/Release
 cp clguetzli/clguetzli.cu.ptx$1 bin/Release/clguetzli/clguetzli.cu.ptx$1
 cp clguetzli/clguetzli.cl bin/Release/clguetzli/clguetzli.cl
